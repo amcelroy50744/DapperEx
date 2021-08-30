45b47276b029d0da8cca39ed60fb235fae916892
amcelroy50744 <85271297+amcelroy50744@users.noreply.github.com>
2021-08-30 13:43:56 -0500

    Add files via upload

---
+++ Dapperdemo/Dapperdemo/DapperDepartmentRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using Dapper;
+
+namespace Dapperdemo
+{
+   public class DapperDepartmentRepository : IDepartmentRepository
+    {
+        private readonly IDbConnection _connection;
+        public DapperDepartmentRepository(IDbConnection connection) 
+        {
+            _connection = connection;
+        }
+        public IEnumerable<Department> GetAllDepartments() 
+        {
+          return  _connection.Query<Department>("Select * From departments");
+        }
+    }
+}
